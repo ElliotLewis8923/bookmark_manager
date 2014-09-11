4463f75c0eed69f9a9820f00f99e1c09fc1f5547
elliotlewis8923 <elewis8923@gmail.com>
2014-09-11 16:35:35 +0100

    passed tenth test, included signing in functionality

--- lib/user.rb
+++ lib/user.rb
@@ -8,7 +8,7 @@ class User
 	attr_accessor :password_confirmation
 
 	validates_confirmation_of :password, :message => "Sorry, your passwords don't match"
-	validates_uniqueness_of :email
+	#validates_uniqueness_of :email
 
 	property :id, Serial
 	property :email, String, :unique => true, :message => "This email is already taken"
@@ -17,6 +17,15 @@ class User
 	def password=(password)
 		@password = password
 		self.password_digest = BCrypt::Password.create(password)
+	end
+
+	def self.authenticate(email, password)
+		user = first(:email => email)
+		if user && BCrypt::Password.new(user.password_digest) == password  # comparison method overridden
+			user
+		else
+			nil
+		end
 	end
 
 end
